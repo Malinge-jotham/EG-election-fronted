f1fc47b6f7587774db7afe9333b43d28cb5deb3b
Malinge-jotham <105794518+Malinge-jotham@users.noreply.github.com>
2025-05-22 03:04:53 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header className="bg-grey-500 text-black py-4">
             <div className="container mx-auto text-center">
-                <h1 className="text-3xl font-bold">Welcome to KUSA Election System</h1>
+                <h1 className="text-3xl font-bold">Welcome to Egerton Univerrsity Election System</h1>
                 <p className="mt-2">Empowering Voters, Building Democracy</p>
             </div>
         </header>
